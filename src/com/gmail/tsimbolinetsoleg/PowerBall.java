477870d3c5b80dbb19cfe80c9b7d8c1a496023ca
Tsimbolinets <tsimbolinetsoleg@gmail.com>
2018-03-08 08:27:53 +0200

    SomeChanges

--- src/com/gmail/tsimbolinetsoleg/PowerBall.java
+++ src/com/gmail/tsimbolinetsoleg/PowerBall.java
@@ -16,7 +16,7 @@ public class PowerBall {
         int ticketsCount = sc.nextInt();
         List<Ticket> tickets = ticket.createTickets(ticketsCount);
         Ticket ticketWinNumber = ticket.registerTicket();
-        /// tickets.add(ticket.registerTicketYourself());  //if u want to add 1 ticket Yourself
+        // tickets.add(ticket.registerTicketYourself());  //if u want to add 1 ticket Yourself
         HashMap<Ticket.possiblePrize, List<Ticket>> winners = ticket.CheckForWinners(tickets,ticketWinNumber);
         ticket.printWinners(winners);
     }
