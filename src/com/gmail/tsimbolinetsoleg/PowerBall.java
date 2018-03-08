07496c4d0a83575a647789529e8221f1f64b3e29
Tsimbolinets <tsimbolinetsoleg@gmail.com>
2018-03-08 08:27:29 +0200

    SomeChanges

--- src/com/gmail/tsimbolinetsoleg/PowerBall.java
+++ src/com/gmail/tsimbolinetsoleg/PowerBall.java
@@ -12,7 +12,7 @@ public class PowerBall {
     public static void main(String[] args) {
         Ticket ticket = new Ticket();
         Scanner sc = new Scanner(System.in);
-        System.out.println("How many tickets do u want to createTickets?");
+        System.out.println("How many tickets do u want to create?");
         int ticketsCount = sc.nextInt();
         List<Ticket> tickets = ticket.createTickets(ticketsCount);
         Ticket ticketWinNumber = ticket.registerTicket();
